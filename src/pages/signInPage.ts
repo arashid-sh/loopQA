7ccf12c7ede14a62d8c270565c198be9e16af276
Affan Rashid <affan.rashid@hearst.com>
2024-04-29 13:16:37 -0700

    fixing login test

--- src/pages/signInPage.ts
+++ src/pages/signInPage.ts
@@ -6,12 +6,14 @@ export class SignInPage {
   readonly emailInputField: Locator;
   readonly passwordInputField: Locator;
   readonly signInButton: Locator;
+  readonly continueButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.emailInputField = page.locator('#email');
     this.passwordInputField = page.locator('#password');
     this.signInButton = page.getByTestId('LoginFormButton');
+    this.continueButton = page.getByTestId('LandingFormButton');
   }
 
   /**
@@ -22,6 +24,7 @@ export class SignInPage {
    */
   async loginUser(email: string, password: string): Promise<void> {
     await this.emailInputField.fill(email);
+    await this.continueButton.click();
     await this.passwordInputField.fill(password);
     await this.signInButton.click();
   }
