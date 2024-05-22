16e2fab085f5fe0e9b64580615318bb9a3ca0ee4
Affan Rashid <affan.rashid@hearst.com>
2024-05-22 12:08:29 -0700

    adding timeout for wait for response

--- src/pages/components/navbar.ts
+++ src/pages/components/navbar.ts
@@ -54,6 +54,6 @@ export class NavBar {
    */
   async clickLink(link: string): Promise<void> {
     await this.page.getByRole('link', { name: `${link}` }).click();
-    await this.page.waitForResponse(/.*ClientProductGalleryQuery.*/);
+    await this.page.waitForResponse(/.*ClientProductGalleryQuery.*/, { timeout: 120000 });
   }
 }
