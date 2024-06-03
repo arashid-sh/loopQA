a64c5eedab12a2e5d5cde1de984a5199c651f1aa
Affan Rashid <affan.rashid@hearst.com>
2024-06-03 12:37:30 -0700

    fixing sign in tests

--- src/tests/signin.spec.ts
+++ src/tests/signin.spec.ts
@@ -13,7 +13,8 @@ test.describe('sign in', () => {
 
   test('validate user can login successfully @MH @SMOKE', async ({ page, signInPage }) => {
     await signInPage.loginUser(process.env.EMAIL!, process.env.PASSWORD!);
-    await expect(page).toHaveTitle("Home | Men's Health Shop");
+
+    await expect(page).toHaveTitle(/.*FastStore.*/);
   });
 
   test('verify error message is shown when invalid password is used', async ({ page, signInPage }) => {
@@ -28,14 +29,13 @@ test.describe('sign in', () => {
   for (const data of config) {
     test(`verify error message when ${data.email} email address is used`, async ({ page, signInPage }) => {
       await signInPage.emailInputField.fill(data.email);
-      await signInPage.continueButton.click();
+      await signInPage.signInButton.click();
       await expect(page.getByText(data.error)).toBeVisible();
     });
   }
 
   test('verify error message when empty password is used', async ({ page, signInPage }) => {
     await signInPage.emailInputField.fill(process.env.EMAIL!);
-    await signInPage.continueButton.click();
     await signInPage.emailInputField.fill('');
     await signInPage.signInButton.click();
     await expect(page.getByText('Password is a required field')).toBeVisible();
@@ -43,14 +43,12 @@ test.describe('sign in', () => {
 
   test('verify clicking "Forgot Password" link redirects the user to Forgot Password page', async ({ page, signInPage }) => {
     await signInPage.emailInputField.fill(process.env.EMAIL!);
-    await signInPage.continueButton.click();
     await signInPage.forgotPasswordLink.click();
     await expect(page).toHaveURL(/forgot-password/);
   });
 
   test('verify user can navigate back to login page from forgot password page', async ({ page, signInPage }) => {
     await signInPage.emailInputField.fill(process.env.EMAIL!);
-    await signInPage.continueButton.click();
     await signInPage.forgotPasswordLink.click();
     await signInPage.returnToSignInLink.click();
     await expect(page).toHaveURL(/login/);
