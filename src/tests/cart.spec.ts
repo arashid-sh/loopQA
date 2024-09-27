09ecc9688fea604bf460264b31f95e6dec501e1f
Affan Rashid <affan.rashid@hearst.com>
2024-09-27 15:02:21 -0700

    fixing last test

--- src/tests/cart.spec.ts
+++ src/tests/cart.spec.ts
@@ -7,9 +7,9 @@ test.describe('Cart', { tag: '@faststore' }, () => {
   test.describe.configure({ timeout: 300000 });
   test('validate message when cart is empty', async ({ page, navBar, productListPage, productDetailsPage, cart }) => {
     await page.goto('/');
-    const productName = "Trayton's Dumbbells";
-    await navBar.searchForProduct(productName);
-    await productListPage.selectProduct(productName);
+    const product = 'sephora';
+    await navBar.searchForProduct(product);
+    await productListPage.selectNthProductFromList(1);
     await productDetailsPage.addQuantityToCart('3');
     // soft assertion to check that the right number of items appear in the mini cart
     expect.soft(await extractNumberFromLocatorTextContent(page.getByTestId('minicart-order-summary-subtotal-label'))).toContain('3');
